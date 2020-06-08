f970bfbf026b9e5e5325f520ffb9b6423e1bb661
Lydia Vandenbergh Jackson <45639445+lvanden@users.noreply.github.com>
2020-06-08 13:46:01 -0400

    light/dark toggle

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 
+ 
+ function toggleMode() {
+  var x = document.getElementById("toggle-light")
+  var y = document.getElementById("toggle-dark")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 
-function toggleMode() {
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+
   var element = document.body;
   element.classList.toggle("dark-mode");
- }
+}
 
- 
